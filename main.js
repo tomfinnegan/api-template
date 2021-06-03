bc0005ef07ea2504659db307c0207e59fc030c62
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-06-03 20:54:49 +0000

    added to page

---
+++ main.js
@@ -0,0 +1,23 @@
+var xhr = new XMLHttpRequest();
+var data;
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
+
+xhr.onreadystatechange = function() {
+    console.log(this.readyState);
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+     
+    }
+};
+
+setTimeout(function() {
+    console.log(data);
+}, 500);
+
