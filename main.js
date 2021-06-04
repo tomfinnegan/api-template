94be0625624cc5e34d4b3408011d79835b253c09
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-06-04 13:41:15 +0000

    added to page

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-var xhr = new XMLHttpRequest();
-var data;
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.send();
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
     console.log(data);
 }
 
-xhr.onreadystatechange = function() {
-    console.log(this.readyState);
-    if (this.readyState == 4 && this.status == 200) {
-      data = JSON.parse(this.responseText);
-     
-    }
-};
-
-setTimeout(function() {
-    console.log(data);
-}, 500);
-
+getData(printDataToConsole);
